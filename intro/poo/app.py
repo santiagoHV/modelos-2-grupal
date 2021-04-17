9dcab8abeb528203bd126c55032601543bb269fa
David Saldarriaga <davidsaldarriaga.pardo@gmail.com>
2021-04-17 17:29:45 -0500

    Terminada reacomodacion templates y sistema de rutas

--- intro/poo/app.py
+++ intro/poo/app.py
@@ -6,12 +6,23 @@ from intro.poo.species.Ogro import Ogro
 from intro.poo.species.Argoniano import Argoniano
 
 app = Flask(__name__)
-personajes_creados = [Imperial('fUmaratto', 180, 200, 19), Breton('Hurtado', 180, 200, 19), Argoniano('Santiago', 180, 200, 19), Ogro('Ebratt', 180, 200, 19)]
+personajes_creados = [Imperial('fUmaratto', 180, 200, 19), Breton('Hurtado', 180, 200, 19),
+                      Argoniano('Santiago', 180, 200, 19), Ogro('Ebratt', 180, 200, 19),
+                      Elfo('Elver', 180, 200, 19)]
 personaje_activo = personajes_creados[0]
 
+
 @app.route('/')
-def hello_world():
-    return render_template('index.html', personajes = personajes_creados)
+def personajes():
+    return render_template('personajes.html', personajes=personajes_creados)
+
+@app.route('/creador')
+def creador():
+    return render_template('creador.html')
+
+@app.route('/ataque')
+def ataque():
+    return render_template('ataque.html', personaje=personaje_activo)
 
 if __name__ == '__main__':
     app.run(debug=True)
