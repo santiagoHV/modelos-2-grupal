5804a89dd738e26b893f562453a6ba74c9c2d142
santiagoHV <santiherreravelas@gmail.com>
2021-05-02 12:07:49 -0500

    retornar valor real iniciado

--- programacion_funcional/CartasPoker.hs
+++ programacion_funcional/CartasPoker.hs
@@ -13,3 +13,10 @@ buscar_carta [] carta = "No esta"
 buscar_carta ((p,v): cs) (p_buscado,v_buscado)
     | p == p_buscado && v == v_buscado = "Si esta"
     | otherwise = buscar_carta cs (p_buscado,v_buscado)
+
+retornar_valor_real::Carta->Int
+retornar_valor_real (palo,valor)
+                | valor == "A" = 1
+                | valor == "J" = 11
+                | valor == "Q" = 12
+                | valor == "K" = 13
